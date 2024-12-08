10d7f07776a966d7445a9c67573a5214270dc009
Cristian Perez <cperezlecaros@gmail.com>
2024-12-08 12:38:53 -0700

    Build header, footer and add styles

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,27 @@
 function Layout(props) {
   const { children } = props;
 
-  const header = <header></header>;
+  const header = (
+    <header>
+      <div>
+        <h1 className="text-gradient">CAFFIEND</h1>
+        <p>For Coffee Insatiates</p>
+      </div>
+      <button>
+        <p>Sign up free</p>
+        <i className="fa-solid fa-mug-hot"></i>
+      </button>
+    </header>
+  );
 
-  const footer = <footer></footer>;
+  const footer = (
+    <footer>
+      <p>
+        <span className="text-gradient">Caffiend</span> was made by{" "}
+        <a href="">Cristian</a> using React and FantaCSS
+      </p>
+    </footer>
+  );
   return (
     <>
       {header}
